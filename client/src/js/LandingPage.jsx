c207cbd48b8884738ccb0bf7f3981f6f30fcc6e7
Evelyn Li <lievelynpl@gmail.com>
2021-01-16 14:15:48 -0500

    basic buttons, connected voice, sms, and verify vonage apis

--- client/src/js/LandingPage.jsx
+++ client/src/js/LandingPage.jsx
@@ -1,8 +1,79 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {Link} from 'react-router-dom';
 import PageContainer from './PageContainer';
+import { Button, Modal } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const LandingComponent = () => {
+  const [show, setShow] = useState(false);
+  const [value, setValue] = useState("");
+  let handleClose = () => setShow(false);
+  
+
+  let sendText = () => {
+    console.log("send text button pressed")
+    let requestOptions = {
+      method: "POST",
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ "message" : "hello there my dear friend. it is nice to see you again."})
+    };
+
+    fetch('/api/sendText', requestOptions)
+    .then(res => res.json())
+    
+  }
+
+  let makeCall = () => {
+    let requestOptions = {
+      method: "POST",
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ "message" : "hello there my dear friend. it is nice to see you again."})
+    };
+
+    fetch('/api/makeCall', requestOptions)
+    .then(res => res.json)
+
+  }
+
+  let verifyButtonPressed = () => {
+    // let requestOptions = {
+    //   method: "POST",
+    //   headers: { 'Content-Type': 'application/json' },
+    //   // body: JSON.stringify({ "message" : "hello there my dear friend. it is nice to see you again."})
+    // };
+
+    fetch('/api/verify/request')
+    .then(res => res.json)
+
+    setShow(true);
+  }
+
+  let handleSubmit = () => {
+    // console.log(value);
+    let requestOptions = {
+      method: "POST",
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ "pin" : `${value}`})
+    };
+
+    fetch('/api/verify/check', requestOptions)
+    .then(res => res.json())
+    .then(res => {
+      console.log(res);
+      if (res) {
+        setShow(false);
+      } else {
+        console.log("Incorrect PIN");
+      }
+    })
+
+    // setShow(false);
+  }
+
+  let handleChange = (e) => {
+    setValue(e.target.value);
+  }
+
   return (
     <div>
         <h2>This is the landing page.</h2>
@@ -12,6 +83,20 @@ const LandingComponent = () => {
         }}>Form page</Link>
         {/* use history.push? not sure if this actually works. https://stackoverflow.com/questions/58506891/how-to-get-data-from-prop-location-state-in-react */}
         <Link to='/comparison/123'>Comparison page</Link>
+        <Button onClick={sendText} type="button">send a text</Button>
+        <Button onClick={makeCall} type="button">make a call</Button>
+        <Button onClick={verifyButtonPressed} type="button">send a code</Button>
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Verify testing</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <label>Enter your verification code below.</label>
+            <input type="number" placeholder="4-digit verification code" value={value} onChange={handleChange}/>
+            <p className="text-muted">Check your phone!</p>
+            <Button type="button" onClick={handleSubmit}>Verify</Button>
+          </Modal.Body>
+        </Modal>
     </div>
   );
 }
--- server/app.js
+++ server/app.js
@@ -3,11 +3,26 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var Nexmo = require('nexmo');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+// vonage setup
+const nexmo = new Nexmo({
+  apiKey: '6c1ba49d',
+  apiSecret: 'Mx95yDBEjmXAFxn8',
+  applicationId: 'ebff6c77-40c2-4357-b46b-7af62d1bf1cd',
+  privateKey: './secret/private.key',
+});
+
+const from = '15877603707';
+const to = '14167799080';
+var text_ctr = 0;
+var call_ctr = 0;
+var request_id = "";
 
 // Serve static files from the React app first
 app.use(express.static(path.join(__dirname, '../client/build')));
@@ -31,6 +46,82 @@ app.get('/api/getList', (req,res) => {
   res.json(list);
   console.log('Sent list of items');
 });
+
+// sends a text
+app.post('/api/sendText', (req, res) => {
+  console.log(req.body)
+  text_ctr += 1;
+  const text = `#${text_ctr}: ${req.body["message"]}`;
+  console.log(text)
+  nexmo.message.sendSms(from, to, text);
+  res.json("text");
+  console.log("sent text");
+  res.status(200).end();
+})
+
+app.post('/api/makeCall', (req, res) => {
+  console.log(req.body["message"]);
+  res.json("vc")
+  call_ctr += 1;
+  var ncco = [
+    {
+      action: 'talk',
+      voiceName: 'Brian',
+      text: req.body["message"] + ` this is call number ${call_ctr}`,
+    },
+  ];
+
+  nexmo.calls.create(
+    {
+      to: [{ type: 'phone', number: to }],
+      from: { type: 'phone', number: from },
+      ncco,
+    },
+    (err, result) => {
+      console.log(err || result);
+    },
+  );
+  
+  console.log("made call");
+})
+
+app.get('/api/verify/request', (req, res) => {
+  console.log("modal opened!");
+  nexmo.verify.request({
+    number: to,
+    brand: "me"
+  }, (err, result) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const verifyRequestId = result.request_id;
+      request_id = verifyRequestId;
+    }
+  });
+  res.status(200).end();
+})
+
+app.post('/api/verify/check', (req, res) => {
+  nexmo.verify.check({
+    request_id: request_id,
+    code: req.body["pin"]
+  }, (err, result) => {
+    if (err) {
+      console.log("error:");
+      console.log(err)
+    } else {
+      console.log(result);
+      if (result.error_text) {
+        // console.log(result.error_text);
+        
+        res.send(false);
+      } else {
+        // console.log("Successfully verified.");
+        res.send(true);
+      }
+    }
+  });
+})
 
 // Return any remaining requests to the React app to handle routing
 app.get('*', (req,res) =>{
